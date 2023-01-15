38dde13234b46ce26b3d4c529d48e60ea986ad2c
doggbmx <manuelorticosta@gmail.com>
2023-01-15 19:18:00 -0300

    adds error handler and generic call in the repository

--- src/feature/user/data/repositories/user_repository_implementation.ts
+++ src/feature/user/data/repositories/user_repository_implementation.ts
@@ -1,40 +1,61 @@
 import { User } from "feature/user/domain/models/User";
 import { UserRepositories } from "./../../domain/repositories/user_repositories";
+import { CustomErrorHandler } from '../../../error/error_handler'
 import { getSourceMapRange } from "typescript";
 
 class userRepositoriesImplementation implements UserRepositories {
+
   async getUser(name?: string): Promise<User> {
-    setTimeout(() => {
-      console.log("Get request");
-    }, 1000);
-    let newUser = {
-      userId: 1,
-      name: "Juan",
-      correo: "lala@gmail.com",
-      password: 12312,
-      userType: "admin",
-    };
-    return newUser as User;
+    return this._call(() => {
+      setTimeout(() => {
+        console.log("Get request");
+      }, 1000);
+      let newUser = {
+        userId: 1,
+        name: "Juan",
+        correo: "lala@gmail.com",
+        password: 12312,
+        userType: "admin",
+      };
+      return newUser as User;
+    }, "Couldn't get the user");
   }
 
   async createdUser(user: User): Promise<Boolean> {
-    setTimeout(() => {
-      console.log("Post request");
-    }, 1000);
-    return true;
+    return this._call(() => {
+      setTimeout(() => {
+        console.log("Post request");
+      }, 1000);
+      return true;
+    }, "Couldn't create the user");
   }
-  async updateUser(user: User): Promise<Boolean> {
-    setTimeout(() => {
-      console.log("update request");
-    }, 1000);
-    return true;
+
+  async updateUser(user:User): Promise<Boolean> {
+    return this._call(() => {
+      setTimeout(() => {
+        console.log("update request");
+      }, 1000);
+      return true;
+    }, "Couldn't update the user")
   }
+
   async deleteUser(user: User): Promise<Boolean> {
-    setTimeout(() => {
-      console.log("delete request");
-    }, 1000);
-    return true;
+    return this._call(() => {
+      setTimeout(() => {
+        console.log("delete request");
+      }, 1000);
+      return true;
+    }, "Couldn't delete the user")
   }
+
+  _call<Type>(callback: Function, errMessage: string): Promise<Type> {
+    try {
+        return callback();
+    } catch (error) {
+        console.log(errMessage);
+        throw CustomErrorHandler.fromGenericError(error as Error);
+    }
+}
 }
 export const userRepositoriesImplementationProvider =
   new userRepositoriesImplementation();
