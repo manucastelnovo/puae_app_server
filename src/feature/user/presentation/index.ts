9f019ec535b4feae2cf99d01be91532b1f070619
manucastelnovo <marnutskate@gmail.com>
2023-02-11 19:05:06 -0300

    add index.ts

---
+++ src/feature/user/presentation/index.ts
@@ -0,0 +1,8 @@
+import { pool } from '../../../core/service/database_service';
+import { PGUsersDataSource } from '../data/data_source/data_source';
+import { UserRepositoriesImplementation } from '../domain/repositories/user_repository_implementation';
+import UsersRouter from './user_router';
+
+const usersDataSource = PGUsersDataSource.create(pool);
+const usersRespository = UserRepositoriesImplementation.create(usersDataSource);
+export const usersRouter = UsersRouter(usersRespository)
