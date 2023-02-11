ea4f6ccdd182baa1d1478fcde7a415da864c4ea1
manucastelnovo <marnutskate@gmail.com>
2023-02-11 19:01:12 -0300

    added routing file

---
+++ src/core/routing/index.ts
@@ -0,0 +1,10 @@
+import express, {Application} from "express"
+import {usersRouter} from '../../feature/user/presentation'
+
+export const configureRouting =(app:Application)=>{
+
+    const router = express.Router();
+    app.use('/api',router);
+    router.use('/users',usersRouter)
+    
+};
