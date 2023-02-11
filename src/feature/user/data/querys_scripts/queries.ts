7ae93365e5d89c4daac24f1a2046a261f005245b
manucastelnovo <marnutskate@gmail.com>
2023-02-11 19:02:38 -0300

    added select and insert query

--- src/feature/user/data/querys_scripts/queries.ts
+++ src/feature/user/data/querys_scripts/queries.ts
@@ -1,1 +1,7 @@
-export const SELECT_USER_QUERY = `SELECT * FROM puae.user WHERE id = $1;`;
+export const SELECT_USER_QUERY = `SELECT * FROM puae.users WHERE name = $1;`;
+
+export const INSERT_USER_QUERY = `
+INSERT INTO puae.users (name, email, password)
+VALUES ($1, $2, $3)
+RETURNING *;
+`
