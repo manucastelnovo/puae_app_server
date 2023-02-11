b7ae93f1bd03888389fe4a8129cdc0ec0dca3589
manucastelnovo <marnutskate@gmail.com>
2023-02-11 19:05:22 -0300

    delete files

--- src/feature/user/presentation/routes/auth.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router: Router = Router();
-import { signup, signin, profile,user } from "./../controllers/auth.controller";
-router.post("/signup", signup);
-router.get("/signin", signin);
-router.get("/profile", profile);
-router.get('/user/:name',user)
-export default router;
