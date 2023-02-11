1bda8ab1ad7cb17a5ff882ebae0239ecf89f9b14
manucastelnovo <marnutskate@gmail.com>
2023-02-11 19:04:20 -0300

    changed the createdUser method for createuser

--- src/feature/user/domain/repositories/user_repositories.ts
+++ src/feature/user/domain/repositories/user_repositories.ts
@@ -2,7 +2,7 @@ import { User } from "feature/user/domain/models/User";
 
 export interface UserRepositories {
   getUser(name: string): Promise<User>;
-  createdUser(user: User): Promise<User>;
+  createUser(user: User): Promise<User>;
   updateUser(user: User): Promise<Boolean>;
   deleteUser(user: User): Promise<Boolean>;
 }
