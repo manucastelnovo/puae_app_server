c55897060ea4d1b32168e1e502c17011a015010d
manucastelnovo <marnutskate@gmail.com>
2023-02-12 17:54:12 -0300

    add databases error

---
+++ src/core/error/databases_error.ts
@@ -0,0 +1,15 @@
+import { CustomError } from './custom_error';
+
+export class DatabaseError extends CustomError {
+    statusCode = 500;
+
+    constructor(public error: Error) {
+      super()
+      Object.setPrototypeOf(this, DatabaseError.prototype);
+    }
+
+    formatErrors(): { message: string; field?: string }[] {
+        return [{message: 'Database error.'}];
+    }
+
+}
