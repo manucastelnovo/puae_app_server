677269dd717a081d09d8c7ee9e6eb3526385af6b
doggbmx <manuelorticosta@gmail.com>
2023-08-22 20:49:25 -0400

    foodRepo baseRepo implementation

--- src/feature/food/domain/repositories/food_repositories.ts
+++ src/feature/food/domain/repositories/food_repositories.ts
@@ -1,7 +1,7 @@
 import { Food } from "feature/food/domain/models/food";
 
 export interface FoodRepositories {
-  getAllFood(userId: number): Promise<Food>;
+  getAllFood(userId: number): Promise<Food[]>;
   createFood(food: Food): Promise<Food>;
   updateFood(food: Food): Promise<Boolean>;
   deleteFood(food: Food): Promise<Boolean>;
