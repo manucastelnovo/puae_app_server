1294bdefb9d23c82a1ddc051735853e09d50bd66
doggbmx <manuelorticosta@gmail.com>
2023-08-22 20:38:43 -0400

    now user_repo extends base_repo

--- src/feature/user/domain/repositories/user_repository_implementation.ts
+++ src/feature/user/domain/repositories/user_repository_implementation.ts
@@ -1,31 +1,37 @@
 import { CreateUser, User } from "feature/user/domain/models/User";
 import { UserRepositories } from "./user_repositories";
 import { UsersDataSource } from "feature/user/data/interfaces/user_data_source";
-import { CustomError } from './../../../../core/error/custom_error';
-import { GenericError } from "./../../../../core/error/generic_error";
+import { BaseRepository } from "core/packages/base_repository";
 
-export class UserRepositoriesImplementation implements UserRepositories {
+export class UserRepositoriesImplementation
+  extends BaseRepository
+  implements UserRepositories
+{
   private usersDataSource: UsersDataSource;
   private constructor(dataSource: UsersDataSource) {
-      this.usersDataSource = dataSource;
+    super();
+    this.usersDataSource = dataSource;
   }
 
   static instance: UserRepositories | null = null;
 
-    static create(dataSource: UsersDataSource) {
-        if (UserRepositoriesImplementation.instance == null) {
-            UserRepositoriesImplementation.instance = new UserRepositoriesImplementation(dataSource);
-        }
-        return UserRepositoriesImplementation.instance;
+  static create(dataSource: UsersDataSource) {
+    if (UserRepositoriesImplementation.instance == null) {
+      UserRepositoriesImplementation.instance =
+        new UserRepositoriesImplementation(dataSource);
     }
+    return UserRepositoriesImplementation.instance;
+  }
 
-  async getUser(name: string): Promise<User>{
+  async getUser(name: string): Promise<User> {
     return await this.callDataSource(async () => {
-          return await this.usersDataSource.getUser(name);
-  })
+      return await this.usersDataSource.getUser(name);
+    });
   }
   async createUser(data: CreateUser): Promise<User> {
-    return await this.callDataSource(() => this.usersDataSource.createUser(data));
+    return await this.callDataSource(() =>
+      this.usersDataSource.createUser(data)
+    );
   }
   updateUser(user: User): Promise<Boolean> {
     throw new Error("Method not implemented.");
@@ -33,19 +39,4 @@ export class UserRepositoriesImplementation implements UserRepositories {
   deleteUser(user: User): Promise<Boolean> {
     throw new Error("Method not implemented.");
   }
-
-  private async callDataSource<T>(callback: Function): Promise<T> {
-        try {
-            return await callback();
-        } catch (error) {
-          if (error instanceof CustomError) {
-            throw error;
-          }
-            throw new GenericError("User Repository error");
-        }
-    }
 }
-
-
-
-
